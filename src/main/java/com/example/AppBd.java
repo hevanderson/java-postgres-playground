92c2e268aabae4f577135510cd66c898f1bd91de
Hevanderson Silva <hevandersons@gmail.com>
2023-08-27 02:51:29 +0000

    bd 2

--- src/main/java/com/example/AppBd.java
+++ src/main/java/com/example/AppBd.java
@@ -7,7 +7,33 @@ import java.sql.SQLException;
 public class AppBd {
     
     public static void main(String[] args) {
-        Connection conn = null;
+        try {
+            Class.forName("org.postgresql.Driver");
+        }catch (ClassNotFoundException e) {
+            // TODO Auto-generated catch block
+           System.err.println("Não foi possível carregar a classe do BD" + e.getMessage());
+        }
+      
+        try {
+            try(var conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "")) {
+                System.out.println("Conectado ao BD");    
+
+                var statement = conn.createStatement();
+                var result = statement.executeQuery("select * from estado");
+
+                while (result.next()) {
+                    System.out.printf("Id: %d Nome: %s UF: %s \n", result.getInt("id"), result.getString("nome"), result.getString("uf"));   
+                }
+
+            } 
+        } catch (SQLException e) {
+            System.err.println("Não foi possível conectar ao BD" + e.getMessage() );
+        }
+      
+      
+      
+      
+        /*   Connection conn = null;
         try {
             Class.forName("org.postgresql.Driver");
             conn = DriverManager.getConnection("jdbc:postgresql://localhost/postgres", "gitpod", "");
@@ -30,7 +56,7 @@ public class AppBd {
             if (conn != null) {
                 conn.close();
             }
-        }
+        } */
     }
 
 }
